e1bc83c9717fa7cf4824e47b6763f435b8b27a80
Sominhwan <thalsghks@naver.com>
2023-08-02 23:55:53 +0900

    20230802 SMH 공지사항페이징쿼리받기

--- src/main/java/kr/co/smh/module/noticeBoard/controller/NoticeBoardController.java
+++ src/main/java/kr/co/smh/module/noticeBoard/controller/NoticeBoardController.java
@@ -1,9 +1,11 @@
 package kr.co.smh.module.noticeBoard.controller;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.smh.module.noticeBoard.dto.NoticeBoardDTO;
@@ -15,8 +17,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(value="/api/v1/noticeboard")
 public class NoticeBoardController {
 	private final NoticeBoardService noticeBoardService;
-	@PostMapping(value="/search")
-	public HttpEntity<?> search(@RequestBody NoticeBoardDTO.ReqNoticeBoard reqDTO) {
+	@PostMapping(value="/write")
+	public HttpEntity<?> noticeBoardWrite(@RequestBody NoticeBoardDTO.ReqNoticeBoard reqDTO) {
 		return noticeBoardService.noticeBoardWrite(reqDTO);			
+	}
+	
+	@GetMapping(value="/search")
+	public String noticeBoardList(@RequestParam("page") String page) {
+		System.out.println(page);
+		return "나는zzz";
 	}
 }
