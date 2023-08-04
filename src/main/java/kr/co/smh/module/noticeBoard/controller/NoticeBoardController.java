c45a13568e4158b32da02eb28004c299b8b99d22
Sominhwan <thalsghks@naver.com>
2023-08-04 23:34:44 +0900

    20230804 SMH log4j2 추가

--- src/main/java/kr/co/smh/module/noticeBoard/controller/NoticeBoardController.java
+++ src/main/java/kr/co/smh/module/noticeBoard/controller/NoticeBoardController.java
@@ -1,5 +1,7 @@
 package kr.co.smh.module.noticeBoard.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +18,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping(value="/api/v1/noticeboard")
 public class NoticeBoardController {
+	 //private Logger log = LogManager.getLogger();
+	private static final Logger log = LogManager.getLogger("kr.co.smh");
+
+
 	private final NoticeBoardService noticeBoardService;
 	@PostMapping(value="/write")
 	public HttpEntity<?> noticeBoardWrite(@RequestBody NoticeBoardDTO.ReqNoticeBoard reqDTO) {
+		log.info("noticeBoardWrtie -->");
 		return noticeBoardService.noticeBoardWrite(reqDTO);			
 	}
 	
